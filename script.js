8ecb5d3d72ea67c81667d78ec216543caeecc668
Pablo Álvarez <79628886+PabloAlvarez-ops@users.noreply.github.com>
2022-10-14 15:50:44 +0000

    Controladores: completados

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
-    // MODELO DE DATOS
+// MODELO DE DATOS
 
-    let mis_peliculas_iniciales = [
-      {titulo: "Superlópez", director: "Javier Ruiz Caldera", "miniatura": "files/superlopez.png"},
-      {titulo: "Jurassic Park", director: "Steven Spielberg", "miniatura": "files/jurassicpark.png"},
-      {titulo: "Interstellar", director: "Christopher Nolan", "miniatura": "files/interstellar.png"}
-    ];
+let mis_peliculas_iniciales = [
+  { titulo: "Superlópez", director: "Javier Ruiz Caldera", "miniatura": "files/superlopez.png" },
+  { titulo: "Jurassic Park", director: "Steven Spielberg", "miniatura": "files/jurassicpark.png" },
+  { titulo: "Interstellar", director: "Christopher Nolan", "miniatura": "files/interstellar.png" }
+];
 
-    localStorage.mis_peliculas = localStorage.mis_peliculas || JSON.stringify(mis_peliculas_iniciales);
+localStorage.mis_peliculas = localStorage.mis_peliculas || JSON.stringify(mis_peliculas_iniciales);
 
-    // VISTAS
-    function indexView(peliculas) {
-    let i = 0;
-    let view = "";
+// VISTAS
+function indexView(peliculas) {
+  let i = 0;
+  let view = "";
 
-    while (i < peliculas.length) {
-        view += `
+  while (i < peliculas.length) {
+    view += `
                 <div class="movie">
                    <div class="movie-img">
                         <img data-my-id="${i}" src="${peliculas[i].miniatura}" onerror="this.src='files/placeholder.png'"/>
@@ -30,18 +30,18 @@
                        <button class="delete" data-my-id="${i}">Borrar</button>
                     </div>
                 </div>\n`;
-        i = i + 1;
-    };
+    i = i + 1;
+  };
 
-    view += `<div class="actions">
+  view += `<div class="actions">
                         <!--Insertar aquí botones de "Añadir" y "Reset"-->
                     </div>`;
 
-    return view;
+  return view;
 }
 
-    function editView(i, pelicula) {
-    return `<h2>Editar Película </h2>
+function editView(i, pelicula) {
+  return `<h2>Editar Película </h2>
                 <div class="field">
                 Título <br>
                 <input  type="text" id="titulo" placeholder="Título" 
@@ -67,10 +67,10 @@
                `;
 }
 
-    const showView = (pelicula) => {
-      // Recibe como argumento el objeto que contiene la información película y con ello, debemos acceder a dos de sus atributos: titulo y director.
+const showView = (pelicula) => {
+  // Recibe como argumento el objeto que contiene la información película y con ello, debemos acceder a dos de sus atributos: titulo y director.
 
-      return `
+  return `
              <p>
 
              La película <b> ${pelicula.titulo} </b> 
@@ -80,12 +80,12 @@
              <div class="actions">
                 <button class="index">Volver</button>
              </div>`;
-    }
+}
 
-    const newView = () => {
-      // 
+const newView = () => {
+  // Vista que permite al usuario introducir una nueva película.
 
-      return `<h2>Crear Película</h2>
+  return `<h2>Crear Película</h2>
                 
                 <div class="field">
                 Título <br>
@@ -109,72 +109,95 @@
                 <div class="actions">
                     <button class="index">Volver</button>
                 </div>`;
-    }
+}
 
 
-    // CONTROLADORES 
-    const indexContr = () => {
-      let mis_peliculas = JSON.parse(localStorage.mis_peliculas);
-      document.getElementById('main').innerHTML = indexView(mis_peliculas);
-    };
+// CONTROLADORES 
+const indexContr = () => {
+  let mis_peliculas = JSON.parse(localStorage.mis_peliculas);
+  document.getElementById('main').innerHTML = indexView(mis_peliculas);
+};
 
-    const showContr = (i) => {
-      //Se encarga de implementar la lógica/funcionalidad de la vista showView(pelicula).
-      
-      let pelicula = JSON.parse(localStorage.mis_peliculas)[i]; 
-      // Inicializamos una variable 'pelicula' donde almacenaremos la información asociada a la pelicula que se encuentre en la posición "i".
-      document.getElementById("main").innerHTML = showView(pelicula); 
-      //Esa pelicula será el argumento de la vista showView(pelicula), que mostrará el título y directo de la película.
+const showContr = (i) => {
+  //Se encarga de implementar la lógica/funcionalidad de la vista showView(pelicula).
 
-    };
+  let pelicula = JSON.parse(localStorage.mis_peliculas)[i];
+  // Inicializamos una variable 'pelicula' donde almacenaremos la información asociada a la pelicula que se encuentre en la posición "i".
+  document.getElementById("main").innerHTML = showView(pelicula);
+  //Esa pelicula será el argumento de la vista showView(pelicula), que mostrará el título y directo de la película.
 
-    const newContr = () => {
-      //Se encarga de implementar la lógica/funcionalidad de la vista newwView().
-      document.getElementById("main").innerHTML = newView(); 
+};
 
-    };
+const newContr = () => {
+  //Se encarga de implementar la lógica/funcionalidad de la vista newwView().
+  document.getElementById("main").innerHTML = newView();
 
-    const createContr = () => {
-      // Completar: controlador que crea una película nueva en el modelo guardado en localStorage
-      // ...
-    };
+};
 
-    const editContr = (i) => {
-      let pelicula = JSON.parse(localStorage.mis_peliculas)[i];
-      document.getElementById('main').innerHTML = editView(i, pelicula);
-    };
+const createContr = () => {
+  // Completar: controlador que crea una película nueva en el modelo guardado en localStorage
+  // ...
+  
+  let mis_peliculas = JSON.parse(localStorage.mis_peliculas);
+  // Analiza la cadena JSON.
+  mis_peliculas[mis_peliculas.length]= {
+      titulo : document.getElementById("titulo").value,
+      director : document.getElementById("director").value,
+      miniatura : document.getElementById("miniatura").value
+  // Pide al usuario que introduzca la información sobre la nueva película.
+  };
+  localStorage.mis_peliculas = JSON.stringify(mis_peliculas);
+  // Transforma el objeto JavaScript a una cadena JSON.
+  indexContr();
+};
 
-    const updateContr = (i) => {
-      let mis_peliculas = JSON.parse(localStorage.mis_peliculas);
-      mis_peliculas[i].titulo = document.getElementById('titulo').value;
-      mis_peliculas[i].director = document.getElementById('director').value;
-      mis_peliculas[i].miniatura = document.getElementById('miniatura').value;
+const editContr = (i) => {
+  let pelicula = JSON.parse(localStorage.mis_peliculas)[i];
+  document.getElementById('main').innerHTML = editView(i, pelicula);
+};
+
+const updateContr = (i) => {
+  let mis_peliculas = JSON.parse(localStorage.mis_peliculas);
+  mis_peliculas[i].titulo = document.getElementById('titulo').value;
+  mis_peliculas[i].director = document.getElementById('director').value;
+  mis_peliculas[i].miniatura = document.getElementById('miniatura').value;
+  localStorage.mis_peliculas = JSON.stringify(mis_peliculas);
+  indexContr();
+};
+
+const deleteContr = (i) => {
+  // Completar:  controlador que actualiza el modelo borrando la película seleccionada
+  // Genera diálogo de confirmación: botón Aceptar devuelve true, Cancel false
+  let mis_peliculas = JSON.parse(localStorage.mis_peliculas);
+  if(window.confirm("¿Seguro que quieres borrar esta película?")){
+      mis_peliculas.splice(i,1);
+    //Eliminamos de la posición "i" un elemento.
       localStorage.mis_peliculas = JSON.stringify(mis_peliculas);
-      indexContr();
-    };
+  indexContr();
 
-    const deleteContr = (i) => {
-      // Completar:  controlador que actualiza el modelo borrando la película seleccionada
-      // Genera diálogo de confirmación: botón Aceptar devuelve true, Cancel false
-    };
+    //window.confirm(): Abre un cuadro de diálogo con el mensaje escrito entre comillas.
+  }
+};
 
-    const resetContr = () => {
-      // Completar:  controlador que reinicia el modelo guardado en localStorage con las películas originales
+const resetContr = () => {
+  // Completar:  controlador que reinicia el modelo guardado en localStorage con las películas originales
+  localStorage.mis_peliculas = JSON.stringify(mis_peliculas_iniciales);
+  indexContr();
 
-    };
+};
 
-    // ROUTER de eventos
-    const matchEvent = (ev, sel) => ev.target.matches(sel);
-    const myId = (ev) => Number(ev.target.dataset.myId);
+// ROUTER de eventos
+const matchEvent = (ev, sel) => ev.target.matches(sel);
+const myId = (ev) => Number(ev.target.dataset.myId);
 
-    document.addEventListener('click', ev => {
-      if (matchEvent(ev, '.index')) indexContr();
-      else if (matchEvent(ev, '.edit')) editContr(myId(ev));
-      else if (matchEvent(ev, '.update')) updateContr(myId(ev));
-      // Completar añadiendo los controladores que faltan
-      else if (matchEvent(ev, ".show")) showContr(myId(ev));
-    })
+document.addEventListener('click', ev => {
+  if (matchEvent(ev, '.index')) indexContr();
+  else if (matchEvent(ev, '.edit')) editContr(myId(ev));
+  else if (matchEvent(ev, '.update')) updateContr(myId(ev));
+  // Completar añadiendo los controladores que faltan
+  else if (matchEvent(ev, ".show")) showContr(myId(ev));
+})
 
 
-    // Inicialización        
-    document.addEventListener('DOMContentLoaded', indexContr);
+// Inicialización        
+document.addEventListener('DOMContentLoaded', indexContr);
